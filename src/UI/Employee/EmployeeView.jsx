c78359b079ad3434d12f7db9506394592a7e663e
samadhism <161122915+samadhism@users.noreply.github.com>
2025-05-23 03:59:36 +0530

    Create the Search bar

--- src/UI/Employee/EmployeeView.jsx
+++ src/UI/Employee/EmployeeView.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-// import styles from "../Employee/EmployeeView.module.css"
+import styles from "../Employee/EmployeeView.module.css"
 import Sidebar from '../../Pages/Sidebar'
+import SearchSection from '../../Pages/SearchSection/SearchSection'
+import Content from '../Content'
+
 
 const EmployeeView = () => {
   return (
     <div>
         <Sidebar/>
+        <div>
+            <SearchSection/>
+            <Content/>
+        </div>
     </div>
   )
 }
