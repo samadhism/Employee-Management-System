5964eefa22e3c8aa04ff515f088a7b85a6ac9016
samadhism <161122915+samadhism@users.noreply.github.com>
2025-05-23 03:16:37 +0530

    Updates of json files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://examination.24x7retail.com',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/api/v1.0'),
+      },
+    },
+  },
+});
+
