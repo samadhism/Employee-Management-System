fbd98933c72a246956c08035ae09ebc0ca92e9ad
samadhism <161122915+samadhism@users.noreply.github.com>
2025-05-23 03:15:22 +0530

    Create Component for Emplyement UI

---
+++ src/UI/Employee/EmployeeView.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../Employee/EmployeeView.module.css"
+
+const EmployeeView = () => {
+  return (
+    <div>EmployeeView</div>
+  )
+}
+
+export default EmployeeView
